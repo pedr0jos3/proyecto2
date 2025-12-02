2605444f19bf731a66f34148a52d0f08fa250464
Juan Andrés <ja.ariasv1@uniandes.edu.co>
2025-12-02 17:47:18 -0500

    cambio de run_server a run

--- app.py
+++ app.py
@@ -308,4 +308,4 @@ def actualizar_graficas(filtro_barrio):
 
 
 if __name__ == "__main__":
-    app.run_server(host="0.0.0.0", port=8050, debug=False)
+    app.run(host="0.0.0.0", port=8050, debug=False)
